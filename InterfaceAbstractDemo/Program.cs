eccaaff20e00fc857817456d1e82b4a96add46fd
Birkankaraer <karaermustafabirkan@gmail.com>
2023-08-31 18:18:16 +0300

    Service Injection

--- InterfaceAbstractDemo/Program.cs
+++ InterfaceAbstractDemo/Program.cs
@@ -15,7 +15,7 @@ namespace InterfaceAbstractDemo
         static void Main(string[] args)
         {
             BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
-            customerManager.Save(new Customer { DateOfBirth = new DateTime(2000,10,18), FirstName = "Mustafa Birkan", LastName = "KARAER", NationalityId = "16918855632" });
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(2000,1,1), FirstName = "Mustafa Birkan", LastName = "KARAER", NationalityId = "11111111111" });
             Console.ReadLine();
         }
     }
