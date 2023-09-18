ef4099174bbd15f4af046f7c89b1d87de9d64512
Birkankaraer <karaermustafabirkan@gmail.com>
2023-09-18 13:45:39 +0300

    18.09.2023 EF

--- EntityFrameworkDemo/Program.cs
+++ EntityFrameworkDemo/Program.cs
@@ -1,13 +1,28 @@
 ﻿namespace EntityFrameworkDemo
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
+        {
+            //GetAll();
+            GetProductsByCategoryId(1);
+        }
+
+        private static void GetAll()
         {
             NorthwindContext context = new NorthwindContext();
             foreach (var product in context.Products)
             {
                 Console.WriteLine(product.ProductName);
+            }
+        }
+        private static void GetProductsByCategoryId(int categoryId)
+        {
+            NorthwindContext northwindcontext = new NorthwindContext();
+            var result = northwindcontext.Products.Where(p => p.CategoryId == categoryId);
+            foreach (var products in result)
+            {
+                Console.WriteLine(products.ProductName);
             }
         }
     }
