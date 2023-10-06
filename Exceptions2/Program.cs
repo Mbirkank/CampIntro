22c11bb9ed4e55a2dd44c0e6fdabe5fa20930b5d
Birkankaraer <karaermustafabirkan@gmail.com>
2023-10-07 00:12:26 +0300

    Exceptions

---
+++ Exceptions2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ExceptionIntro();
+            try
+            {
+                Find();
+            }
+            catch(RecordNotFountException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch(Exception exception)
+            {
+
+            }
+            HandleException(() =>
+            {
+                Find();
+            });
+
+            Console.ReadLine();
+            
+        }
+
+        private static void HandleException(Action aciton)
+        {
+            
+            try
+            {
+                aciton.Invoke();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+        }
+
+        private static void Find()
+        {
+            List<string> students = new List<String>
+            { "Birkan", "Engin", "Fatih" };
+
+            if (!students.Contains("Ahmet"))
+            {
+                throw new RecordNotFountException("Record Not Found!");
+            }
+            else
+            {
+                Console.WriteLine("Record Found!");
+            }
+        }
+
+        private static void ExceptionIntro()
+        {
+            try
+            {
+                string[] students = new string[3] { "Mustafa", "Birkan", "Ahmet" };
+                students[3] = "Ali";
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (DivideByZeroException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                //Console.WriteLine(exception.InnerException);
+            }
+        }
+    }
+}
