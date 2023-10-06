2800fed9ca007f4e1a7a3c64836ad06366512556
Birkankaraer <karaermustafabirkan@gmail.com>
2023-10-06 10:01:37 +0300

    Attributes, AllowMultiple

--- Attributes/Program.cs
+++ Attributes/Program.cs
@@ -10,14 +10,55 @@ namespace Attributes
     {
         static void Main(string[] args)
         {
+            Customer customer = new Customer 
+            { 
+                Id = 1,
+                LastName = "Karaer", 
+                Age = 23
+            };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadLine();
         }
     }
+    [ToTable("Customers")]
+    [ToTable("TblCustomers")]
     class Customer
     {
         public int Id { get; set; }
         [RequiredProperty]
         public string FirstName { get; set; }
+        [RequiredProperty]
         public string LastName { get; set; }
+        [RequiredProperty]
         public int Age { get; set; }
     }
+    class CustomerDal
+    {
+        [Obsolete("Don't use Add, instead use AddNew method")]
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added",customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added", customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+    }
+    //Bu attributü herkese kullanabilirsin.
+    [AttributeUsage(AttributeTargets.Property)]
+    class RequiredPropertyAttribute:Attribute 
+    {
+
+    }
+    [AttributeUsage(AttributeTargets.Class,AllowMultiple = true)]
+    class ToTableAttribute : Attribute
+    {
+        string _tableName;
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+    }
+
 }
